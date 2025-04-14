4ba9f8bb45a84788f314f52fd6e40eb851a4a11c
kai matsubara <k022g0104@g.neec.ac.jp>
2025-04-14 17:10:20 +0900

    fix

--- Game/Scene/DemoScene.cpp
+++ Game/Scene/DemoScene.cpp
@@ -16,7 +16,7 @@ void DemoScene::Init()
 	wood_ = std::make_unique<WorldDesign>();
 	wood_->Init({ 1.0f,1.0f,1.0f }, { 0.0f,15.0f,30.0f }, "worldDesign");
 	map_ = std::make_unique<map>();
-	map_->Init({ 1.0f,1.0f,1.0f }, { 10.0f,50.0f,30.0f }, "map");
+	map_->Init({ 100.0f,100.0f,100.0f }, { 100.0f,50.0f,30.0f }, "map");
 
 	
 	camera_ = std::make_unique<Camera>();
